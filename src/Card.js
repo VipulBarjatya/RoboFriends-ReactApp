c7e05ee88ce591d300b6a125d61efe1d0d414065
Vipul <vipulbarjatya@gmail.com>
2022-10-28 14:00:49 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <img src="https://robohash.org/test?100*100" alt="robot" />
+      <h2>Name here</h2>
+      <p>name@email.com</p>
+    </>
+  );
+};
+
+export default Card;
